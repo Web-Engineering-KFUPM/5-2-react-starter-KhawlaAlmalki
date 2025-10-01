76f6952c13d63761e32d994955c4aa16c05f214c
KhawlaAlmalki <s202247320@kfupm.edu.sa>
2025-10-01 12:13:21 +0300

    Task 1

--- 5-2-react-starter/src/App.jsx
+++ 5-2-react-starter/src/App.jsx
@@ -1,20 +1,12 @@
-import './App.css'
+import StudentCard from './components/StudentCard';
 
 function App() {
-  return (
-    <div className="app">
-      <header className="dashboard-header">
-        <h1>Student Information Dashboard</h1>
-        <p>View and manage student details</p>
-      </header>
-
-      <main className="dashboard-main">
-        <div className="cards-container">
-          {/* TODO: Import and render StudentCard components here */}
+    return (
+        <div>
+            <h1>Student Info</h1>
+            <StudentCard />
         </div>
-      </main>
-    </div>
-  )
+    );
 }
 
-export default App
+export default App;
---
+++ 5-2-react-starter/src/components/StudentCard.jsx
@@ -0,0 +1,11 @@
+function StudentCard() {
+    return (
+        <div>
+            <h3>Name: Khawlah Almalki</h3>
+            <p>Student ID: 202247320</p>
+            <p>Department: ICS</p>
+        </div>
+    );
+}
+
+export default StudentCard;
